3cb1f01682d9bae05eed4912526631a6520ec959
Nikit-ac <nikita.arar@mail.ru>
2016-03-15 20:23:06 +0200

    3

--- response.php
+++ response.php
@@ -1,13 +1,16 @@
 <?php
-  $data1 = $_POST[d];
+  $data = json_decode($_POST[d]);
   // $data = explode(",", $data1);
-  $data1 = substr($data1, 1);
-  $data1 = substr($data1, 0, -1);
+  // $data1 = substr($data1, 1);
+  // $data1 = substr($data1, 0, -1);
 
-echo var_dump($data1);
-  // $fp = fopen('file.csv', 'a');
-  // foreach ($data as $fields) {
-  //   fputcsv($fp, $fields);
-  // }
-  // fclose($fp);
+// echo var_dump($data1);
+  $fp = fopen('file.csv', 'a');
+  foreach ($data as $phrase) {
+    if ($phrase) {
+      $phrase = array($phrase);
+      fputcsv($fp, $phrase);
+    }
+  }
+  fclose($fp);
 ?>
